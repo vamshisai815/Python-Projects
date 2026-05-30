57bb45d40ac82b77d7fd21d956d84b8c3f5b0287
vamshisai815 <vsai16988@gmail.com>
2026-05-31 00:37:52 +0530

    Added in Calcualtor

---
+++ BMI Calculator.py
@@ -0,0 +1,9 @@
+#BMI CACULATOR 
+#?Project objective 
+#! Bulid a Simple BMI Calculator System that:
+#? Takes user height and weight and calcuates BMI Value
+#? Determine health category and Displays BMI Summary 
+#TODO= TASK LIST 
+#!1 Show welcome message and BMI Calculator 
+#!2 Ask the user to enter your details 
+#!3 Convert height and weight 
